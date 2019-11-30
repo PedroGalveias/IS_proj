1bdfafe6485e93cea462a56165648ad6c6e984f0
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-11-30 13:46:07 +0000

    Added more data files, We can now read the WHOLE data set weeeeeeeeeee uwu

--- ISVisualSolution/Bridge/Bridge.py
+++ ISVisualSolution/Bridge/Bridge.py
@@ -1,21 +1,26 @@
 import struct
-import numpy as np
 import os
+from datetime import datetime
 
-def bread(filename):
+def execute_brige(filename):
 	with open(filename, 'rb') as f:
 		while True:
 			bytes = f.read(24)
 			if bytes == b'':
 				break
 			else:
-				id, temp, hum, batt, time, o = struct.unpack('=iffili', bytes)
+				id, p3, p4, temp, hum, batt, p1, p2, time, o = struct.unpack('=bbhffbbhli', bytes)
 				print("--------------------------" + os.linesep)
 				print("ID: " + str(id) + os.linesep)
 				print("Temperature: " + str(temp) + os.linesep)
 				print("Hummidity: " + str(hum) + os.linesep)
 				print("Battery: " + str(batt) + os.linesep)
-				print("Timestamp: " + str(time) + os.linesep)
+				print("Timestamp: " + str(time) + " (" + str(datetime.fromtimestamp(time)) + ")" + os.linesep)
 				print("--------------------------" + os.linesep)
 
-bread("data.bin")
+
+def main():
+	execute_brige("data_1.bin")
+
+if __name__ == "__main__":
+	main()
