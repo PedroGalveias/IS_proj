f161a621c90d8245b7b96faa2d7c2cafbf9087be
kenalx1999 <48328690+kenalx1999@users.noreply.github.com>
2019-12-02 16:28:29 +0000

    Inicial commit
    Crie o Alert module => como Form

---
+++ ISVisualSolution/AlertsForm/AlertForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlertsForm
+{
+    public partial class AlertForm : Form
+    {
+        public AlertForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
