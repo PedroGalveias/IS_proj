786a53474d880074ebf35ef83db6ab98c69f5458
Andres <andrestoledo@outlook.pt>
2019-12-02 18:34:00 +0000

    ja faz a conecao com o Mosquito, ainda por fazer os comandos para criar a tabelas

--- ISVisualSolution/BaseDados/Program.cs
+++ ISVisualSolution/BaseDados/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Newtonsoft.Json;
 
 namespace BaseDados
 {
+
     class Program
     {
+        public static string conn = Properties.Settings.Default.ConnectionString;
+        public static string[] topics = { "news" };
         static void Main(string[] args)
         {
+            Console.Write("Escreva um ip: ");
+            string ip = Console.ReadLine();
+            ip.Trim();
+            ip = "127.0.0.1";
+            Console.WriteLine("Seu ip é: " + ip);
+            MqttClient mClient = new MqttClient(ip);
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Broker OK...");
+            }
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE };
+            mClient.Subscribe(topics, qosLevels);
+
+            mClient.MqttMsgPublishReceived += MClient_MqttMsgPublishReceived;
+          
+            Console.ReadKey();
+        }
+
+        private static void MClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string msg = Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine($"Topico:{e.Topic}|Msg:{msg}");
+            dynamic response=JsonConvert.DeserializeObject(msg);
+            Console.WriteLine("json: " + response);
+            
         }
     }
 }
