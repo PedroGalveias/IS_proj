f459991f2d4b6b41be9da94c4e5173cc6cc782fa
Thainá Braz <thainabbraz@gmail.com>
2019-12-02 17:41:49 +0000

    API beginning

---
+++ ISVisualSolution/API/Models/Sensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Sensor
+    {
+        public int Id { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public int Battery { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
