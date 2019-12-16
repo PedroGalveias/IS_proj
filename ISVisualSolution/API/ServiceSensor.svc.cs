d254c52db9266cefc14d12e7cee6474778a3b925
Thainá Braz <thainabbraz@gmail.com>
2019-12-16 16:33:58 +0000

    stash

--- ISVisualSolution/API/ServiceSensor.svc.cs
+++ ISVisualSolution/API/ServiceSensor.svc.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace API
 {
+<<<<<<< HEAD
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class ServiceSensor : ISensor
@@ -22,4 +23,20 @@ namespace API
             throw new NotImplementedException();
         }
     }
+=======
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class ServiceSensor : ISensor
+	{
+		public List<Sensor> GetAllSensors()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Sensor GetSensorById()
+		{
+			throw new NotImplementedException();
+		}
+	}
+>>>>>>> c39556fa6ad9c6ed7e51ae7e39151a36eadeb943
 }
