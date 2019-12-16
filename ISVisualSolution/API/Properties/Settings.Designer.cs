64ca3840fe8ec52306974db45c0017ad9536e08b
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-12-16 16:26:28 +0000

    DB Fix

--- ISVisualSolution/API/Properties/Settings.Designer.cs
+++ ISVisualSolution/API/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace API.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=Tru" +
-            "e")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=IS_DB;Integrated Security=True" +
+            "")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
